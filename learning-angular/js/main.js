3d4be873449e419f14e20c9715a9291a3a72e4ba
clarammdantas <clara.moraesd@gmail.com>
2017-07-10 17:47:18 -0300

    bla

--- learning-angular/js/main.js
+++ learning-angular/js/main.js
@@ -1,4 +1,6 @@
 var app = angular.module('myApp', []);
+var apiKey = 'ljmKWhYdyrkvuZoD7KZFfM75iCUzrBsQIgzzxpMy';
+var nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
 app.controller('PlayerController', ['$scope', function($scope) {
   $scope.playing = false;
@@ -20,8 +22,39 @@ app.controller('PlayerController', ['$scope', function($scope) {
       $scope.stop()
     });
   });
+
+  $http({
+    method: 'JSONP',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK';
+  }).success(function(data, status){
+    $scope.programs = data.list.story;
+  }).error(function(data, status){
+
+  });
+
 }]);
+
+// examples
+app.controller('MyController', function($scope){
+  $scope.person = {name: "Luisa"};
+
+  var updateClock = function() {
+    $scope.clock = new Date();
+  };
+
+  var timer = setInterval(function(){
+    $scope.$apply(updateClock);
+  }, 1000);
+
+  updateClock();
+});
 
 app.controller('RelatedController', ['$scope', function($scope) {
 
 }]);
+
+app.controller('DemoController', fucntion($scope){
+  $scope.counter = 0;
+  $scope.add = function(amount) { $scope.counter += amount; };
+  $scope.sub = function(amount) { $scope.counter -= amount; };
+});
